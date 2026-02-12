1de3c41e18853c654bb138822e426953853f8cf9
Knuti-exe <57a149246@gmail.com>
2026-02-12 09:43:30 +0100

    Better logs, light-sleep mode and temp publication fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,53 @@
 #include <PubSubClient.h>
 #include <ESPTelnet.h>
 #include <ArduinoOTA.h>
+#include <esp_wifi.h>
+#include <esp_system.h>
 
 #define relay 2
 
 // WiFi
-const char *ssid = "*********";
-const char *passwd = "*******";
+static const char *ssid = "TP-Link_5235";
+static const char *passwd = "MaDaPi16";
 // MQTT
-const char *mqtt_server = "192.168.0.100";  // don't forget, that (in my case) it's running on MQTT
+static const char *mqtt_server = "192.168.0.100";  // don't forget, that (in my case) it's running on MQTT
                                             // v. 3.1
-const int mqtt_port = 1883;
-const char *mqtt_user = "xiao0";
-const char *mqtt_passwd = "broker#1234";
-const char *mqtt_topic_sw = "xiao_light/relay";
-const char *mqtt_topic = "xiao_light/lightbulb";
-const char *temp_topic = "xiao_light/temp";
+static const int mqtt_port = 1883;
+static const char *mqtt_user = "xiao0";
+static const char *mqtt_passwd = "broker#1234";
+static const char *mqtt_topic_sw = "xiao_light/relay";
+static const char *mqtt_topic = "xiao_light/lightbulb";
 // Telnet
-const int telnet_port = 23;
+static const int telnet_port = 23;
 
-long long int last_time = 0;
-bool force_refresh = false;
-bool lightbulb_state = false;
-bool telnet_connected = false;
-long long int last_recon_time = 0;
-bool isUpdating = false;
+static bool force_refresh = false;
+static bool lightbulb_state = false;
+static bool isUpdating = false;
+static int64_t last_temp_check = 0;
+static int64_t last_recon_time = 0;
+static int64_t last_temp_info = 0;
+static float coreTemp = 0.0;
+
+// Font colors
+const static char* CLR_RST = "\033[0m";
+const static char* CLR_RED = "\033[31m";
+const static char* CLR_GRN = "\033[32m";
+const static char* CLR_YLW = "\033[33m";
 
 WiFiServer telnetServer(telnet_port);
-WiFiClient telnetClient;
+WiFiClient telnet;
 
 WiFiClient client;
 PubSubClient mqttClient(client);
 
 void reconnect();
 void callback(char *topic, byte *payload, unsigned int length);
+const char* getResetReason();
 
 
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, passwd);
-  // TODO : wdrozyc strone WWW logowania do sieci WIFI 
 
   while (WiFi.status() != WL_CONNECTED) {
     vTaskDelay(pdMS_TO_TICKS(200));
@@ -55,6 +63,8 @@ void setup() {
   
   mqttClient.setServer(mqtt_server, mqtt_port);
   mqttClient.setCallback(callback);
+
+  esp_wifi_set_ps(WIFI_PS_MAX_MODEM);        // light-sleep mode
 
   ArduinoOTA.begin();
 
@@ -71,14 +81,16 @@ void loop() {
   ArduinoOTA.handle();
 
   if (!isUpdating) {
-    if (!mqttClient.connected() && millis() - last_recon_time > 5000) {
+    int64_t now = esp_timer_get_time();
+
+    if (!mqttClient.connected() && now - last_recon_time > 5 * 1000000) { // 5 sec
       reconnect();
-      last_recon_time = millis();
+      last_recon_time = now;
     }
 
     mqttClient.loop();
 
-    if (millis() - last_time > 10000 || force_refresh) {
+    if (force_refresh) {
       
       String lightbulb_msg;
       String lightbulb_sw_msg;
@@ -93,38 +105,50 @@ void loop() {
       
       }
 
-      mqttClient.publish(mqtt_topic, lightbulb_msg.c_str());
-
-      // just to check if temps are fine in closed box
-      float coreTemp = temperatureRead();
-      mqttClient.publish(temp_topic, String(coreTemp).c_str());
-
-      if (coreTemp > 90.0) {
-        Serial.println("WARNING : OVERHEAT! Turning into sleep mode for 5 mins...");
-        esp_sleep_enable_timer_wakeup(300 * 1000000);
-        esp_deep_sleep_start();
-      }
+      mqttClient.publish(mqtt_topic, lightbulb_msg.c_str());      
 
       force_refresh = false;
-      last_time = millis();
     }
 
-    if ((!telnetClient || !telnetClient.connected()) && !telnet_connected) {
+    now = esp_timer_get_time();
+
+    if (now - last_temp_info > 60 * 1000000) telnet.printf("MCU's temperature: %.4f", 
+      coreTemp);
+
+    if (now - last_temp_check > 5 * 1000000) {
+
+      coreTemp = temperatureRead();
+
+      if (coreTemp > 90.0) {
+        telnet.printf("%sWARNING%s: \t%sOVERHEAT!%s Turning into sleep mode for 5 mins...\n\r", 
+        CLR_YLW, CLR_RST, CLR_RED, CLR_RST);
+
+        esp_sleep_enable_timer_wakeup(300000000);
+        esp_deep_sleep_start();
+      }
+    
+      last_temp_check = esp_timer_get_time();
+
+    }
+
+    if (!telnet || !telnet.connected()) {
         
-        telnetClient = telnetServer.accept();
+        telnet = telnetServer.accept();
         
-        if (telnetClient) {
+        if (telnet) {
         
-          telnetClient.println("Telnet client have just connected!");
-          Serial.println("Telnet client have just connected!");
-          telnetClient.println("...\r\n");
-          telnet_connected = true;
-        
-        }
+          telnet.print("INFO: \tTelnet client have just connected!\n\r");
+          Serial.print("Telnet client have just connected!\n\r");
+          telnet.print("...\n\r");
+
+          telnet.printf("%sWARNING%s: \tLast reset reason: %s%s%s\n\r",
+            CLR_YLW, CLR_RST, CLR_YLW, getResetReason(), CLR_RST);
+          
+        } 
       }
     
 
-    vTaskDelay(pdMS_TO_TICKS(10));
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
 
   
@@ -134,28 +158,30 @@ void reconnect() {
 
   bool connected = false;
 
-  Serial.println("Reconnecting with MQTT broker...");
-  telnetClient.println("\rReconnecting with MQTT broker...");
+  Serial.printf("%sWARNING%s: Reconnecting with MQTT broker...\n\r", CLR_YLW, CLR_RST);
+  telnet.printf("%sWARNING%s: Reconnecting with MQTT broker...\n\r", CLR_YLW, CLR_RST);
 
 
   if (mqttClient.connect("xiao", mqtt_user, mqtt_passwd)) {
-    Serial.println("Connected.");
-    telnetClient.println("Connected.");
+    Serial.printf("%sConnected.%s", CLR_GRN, CLR_RST);
+    telnet.printf("%sConnected.%s\n\r", CLR_GRN, CLR_RST);
 
     mqttClient.subscribe(mqtt_topic_sw);
     
   } else {
     
-    Serial.printf("Error : %i", mqttClient.state());
-    telnetClient.printf("Error : %i", mqttClient.state());
+    Serial.printf("%sError%s: %i\n\r", CLR_RED, CLR_RST, mqttClient.state());
+    telnet.printf("%sError%s: %i\n\r", CLR_RED, CLR_RST, mqttClient.state());
 
-    
   }
   
   
   if (!connected) {
-    Serial.println("Trying again later...");
-    telnetClient.println("Trying again later...");
+    Serial.printf("%sWARNING%s: Couldn't connect. Trying again later...\n\r", 
+      CLR_YLW, CLR_RST);
+    
+    telnet.printf("%sWARNING%s: Couldn't connect. Trying again later...\n\r", 
+      CLR_YLW, CLR_RST);
   }  
   
 }
@@ -164,7 +190,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
 
   String msg;
 
-  for (int i=0; i<length; i++) {
+  for (uint i=0; i<length; i++) {
     msg += (char)payload[i];
   }
 
@@ -176,8 +202,8 @@ void callback(char *topic, byte *payload, unsigned int length) {
       lightbulb_state = true;
       force_refresh = true;
 
-      Serial.printf("Light ON\n");
-      telnetClient.printf("\rLight ON\n");
+      telnet.printf("INFO: \tLight %sON%s\n\r", CLR_GRN, CLR_RST);
+      Serial.printf("INFO: \tLight %sON%s\n", CLR_GRN, CLR_RST);
 
     } else if (msg == "off") {
 
@@ -185,14 +211,28 @@ void callback(char *topic, byte *payload, unsigned int length) {
       lightbulb_state = false;
       force_refresh = true;
 
-      Serial.printf("Light OFF\n");
-      telnetClient.printf("\rLight OFF\n");
+      telnet.printf("INFO: \tLight %sOFF%s\n\r", CLR_RED, CLR_RST);
+      Serial.printf("INFO: \tLight %sOFF%s\n", CLR_RED, CLR_RST);
+
 
     } else {
 
-      telnetClient.printf("\r\nUnexpected msg : %s \n", msg.c_str());
-      Serial.printf("\nUnexpected msg : %s \n", msg.c_str());
+      telnet.printf("%sERROR%s: \tUnexpected msg - got: %s \n", CLR_RED, CLR_RST, msg.c_str());
+      Serial.printf("%sERROR%s: \tUnexpected msg - got: %s \n", CLR_RED, CLR_RST, msg.c_str());
+
     }
   }
 
+}
+
+const char* getResetReason() {
+    switch (esp_reset_reason()) {
+        case ESP_RST_POWERON: return "Power on reset";
+        case ESP_RST_SW:      return "Software reset";
+        case ESP_RST_PANIC:   return "Exception / Panic reset";
+        case ESP_RST_INT_WDT: return "Interrupt Watchdog";
+        case ESP_RST_TASK_WDT:return "Task Watchdog";
+        case ESP_RST_BROWNOUT:return "Voltage dip";
+        default:              return "Other";
+    }
 }
